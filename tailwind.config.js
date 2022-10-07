83a14fb2c629c6a1921df74cad9456a2f8fea5eb
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-10-07 21:50:02 +0600

    dlt darkTheme: "dark"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,60 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/**/*.{html,js,jsx,ts,tsx}",
+
+//     'node_modules/daisyui/dist/**/*.js', 'node_modules/react-daisyui/dist/**/*.js'
+
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+
+//    // add daisyUI plugin
+//   // plugins: [
+//   //   require('daisyui'),
+//   //   require("@tailwindcss/typography")
+//   // ],
+  
+//   // daisyUI config (optional)
+//   daisyui: {
+//     styled: true,
+//     themes: true,
+//     base: true,
+//     utils: true,
+//     logs: true,
+//     rtl: false,
+//     prefix: ""
+    
+//   }
+// }
+
+
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
-
-    'node_modules/daisyui/dist/**/*.js', 'node_modules/react-daisyui/dist/**/*.js'
-
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-
-   // add daisyUI plugin
-  plugins: [
-    require('daisyui'),
-    require("@tailwindcss/typography")
-  ],
-  
-  // daisyUI config (optional)
+  //.....................................................
   daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: ""
-    
-  }
+    themes: [
+      {
+        mytheme: {
+          primary: "#669999",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+    //....................................................
+  },
+  plugins: [
+    require("daisyui"),
+    require("@tailwindcss/typography")
+],
+
 }
