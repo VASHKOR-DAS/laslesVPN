ab6721891813004f6845db67811a220f7b3ae997
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-10-09 19:47:49 +0600

    Card section fully complete with wrap

--- src/Components/Pages/Card/Card.js
+++ src/Components/Pages/Card/Card.js
@@ -8,7 +8,7 @@ const Card = () => {
     return (
         <>
 
-            <article className='prose prose-sm md:prose-base lg:prose-xl text-center max-w-none'>
+            <article className='text-center prose-sm md:prose-base lg:prose-lg max-w-none'>
                 <h3 className="font-bold">Choose Your Plan</h3>
                 <p>Let's choose the package that is best for you and explore it happily and cheerfully.</p>
             </article>
@@ -19,34 +19,27 @@ const Card = () => {
 
             {/* Card Section Start */}
 
-            <div className='grid lg:grid-flow-col justify-items-center gap-5 md:gap-5 lg:gap-0'>
+            <div className='flex flex-wrap text-center justify-center gap-6 lg:gap-10'>
+
 
                 {/* -----------Card 1 start----------- */}
-                <div className='grid justify-items-center
-                border-2 border-current-500 rounded-lg py-9 px-16 w-max
+                <div className='border-2 border-current-500 rounded-lg px-16 
+                flex justify-between flex-col pb-10 
                 '>
 
-                    <img className='w-32' src={FreePlan} alt="logo" srcSet="" />
+                    <article className='prose-sm md:prose-base'>
 
-                    <article className='prose prose-sm md:prose-base max-w-none text-center'>
+                        <img className='w-32 block mx-auto' src={FreePlan} alt="logo" srcSet="" />
 
                         <h4 className='font-bold pb-5 pt-6'>Free Plan</h4>
 
                         <li className='flex'>
 
-                            <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-4 fill-green-600">
-                                    <path fillRule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clipRule="evenodd" />
-                                </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-4 fill-green-600">
+                                <path fillRule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clipRule="evenodd" />
+                            </svg>
 
-                            </div>
-
-
-                            <div>
-
-                                Unlimited Bandwidth
-                            </div>
-
+                            Unlimited Bandwidth
                         </li>
 
                         <li className='flex'>
@@ -73,40 +66,43 @@ const Card = () => {
                             Works on All Devices
                         </li>
 
-                        <h3 className='font-bold'>Free</h3>
-
                     </article>
 
-                    <a href='/' className="
-                    mt-4
-                    px-16
-                    py-2
-                    text-sm
-                    no-underline
-                    font-bold
-                    border-[3px]
-                    text-[#F53835] 
-                    hover:bg-orange-600
-                    hover:border-orange-600
-                    focus:ring
-                    focus:ring-orange-300
-                    btn-outline rounded-3xl 
-                    normal-case">Select</a>
+
+                    <div className="prose-sm md:prose-base order-last mt-6">
+                        <h3 className=' font-bold pb-4'>Free</h3>
+                        <a href='/' className="
+                            px-16
+                            py-2
+                            text-sm
+                            no-underline
+                            font-bold
+                            border-[3px]
+                            text-[#F53835] 
+                            hover:bg-orange-600
+                            hover:border-orange-600
+                            focus:ring
+                            focus:ring-orange-300
+                            btn-outline rounded-3xl 
+                            normal-case">Select
+                        </a>
+                    </div>
 
                 </div>
                 {/* -----------Card 1 end----------- */}
 
 
                 {/* -----------Card 2 start----------- */}
-                <div className='grid justify-items-center
-            border-2 border-current-500 rounded-lg py-9 px-16 w-max
-            '>
+                <div className='border-2 border-current-500 rounded-lg px-16 
+                flex justify-between flex-col pb-10
+                '>
 
-                    <img className='w-32' src={StandardPlan} alt="logo" srcSet="" />
+                    <article className='prose-sm md:prose-base'>
 
-                    <article className='prose prose-sm md:prose-base max-w-none text-center'>
+                        <img className='w-32 block mx-auto' src={StandardPlan} alt="logo" srcSet="" />
 
-                        <h4 className='font-extrabold pb-5 pt-6'>Standard Plan</h4>
+                        <h4 className='font-bold pb-5 pt-6'>Standard Plan</h4>
+
 
                         <li className='flex'>
 
@@ -149,40 +145,54 @@ const Card = () => {
                             Works on All Devices
                         </li>
 
-                        <h3 className='font-bold'>Free</h3>
+                        <li className='flex'>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-4 fill-green-600">
+                                <path fillRule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clipRule="evenodd" />
+                            </svg>
+
+                            Connect Anywhere
+                        </li>
+
 
                     </article>
 
-                    <a href='/' className="
-                    mt-4
-                    px-16
-                    py-2
-                    text-sm
-                    no-underline
-                    font-bold
-                    border-[3px]
-                    text-[#F53835] 
-                    hover:bg-orange-600
-                    hover:border-orange-600
-                    focus:ring
-                    focus:ring-orange-300
-                    btn-outline rounded-3xl 
-                    normal-case">Select</a>
+
+                    <div className="prose-sm md:prose-base order-last mt-6">
+                        <h3 className='font-bold pb-4'>
+                            <span className='font-extrabold'>&#36;</span>9 <span className='text-gray-500 font-semibold'>/ mo</span>
+                        </h3>
+                        <a href='/' className="
+                            px-16
+                            py-2
+                            text-sm
+                            no-underline
+                            font-bold
+                            border-[3px]
+                            text-[#F53835] 
+                            hover:bg-orange-600
+                            hover:border-orange-600
+                            focus:ring
+                            focus:ring-orange-300
+                            btn-outline rounded-3xl 
+                            normal-case">Select
+                        </a>
+                    </div>
 
                 </div>
                 {/* -----------Card 2 end----------- */}
 
 
                 {/* -----------Card 3 start----------- */}
-                <div className='grid justify-items-center
-            border-2 border-current-500 rounded-lg py-9 px-16 w-max
-            '>
+                <div className='border-2 border-current-500 rounded-lg px-16 
+                flex justify-between flex-col pb-10
+                '>
 
-                    <img className='w-32' src={PremiumPlan} alt="logo" srcSet="" />
+                    <article className='prose-sm md:prose-base'>
 
-                    <article className='prose prose-sm md:prose-base max-w-none text-center'>
+                        <img className='w-32 block mx-auto' src={PremiumPlan} alt="logo" srcSet="" />
 
                         <h4 className='font-bold pb-5 pt-6'>Premium Plan</h4>
+
 
                         <li className='flex'>
 
@@ -225,28 +235,46 @@ const Card = () => {
                             Works on All Devices
                         </li>
 
-                        <h3 className='font-bold'>
-                        <span className=''></span>12
-                        </h3>
+                        <li className='flex'>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-4 fill-green-600">
+                                <path fillRule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clipRule="evenodd" />
+                            </svg>
+
+                            Connect Anywhere
+                        </li>
+
+                        <li className='flex'>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-4 fill-green-600">
+                                <path fillRule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clipRule="evenodd" />
+                            </svg>
+
+                            Get New Features
+                        </li>
 
                     </article>
 
-                    <a href='/' className="
-                    mt-4
-                    px-16
-                    py-2
-                    text-sm
-                    no-underline
-                    font-bold
-                    border-[3px]
-                    text-white 
-                    bg-[#F53835]
-                    hover:bg-orange-600
-                    hover:border-orange-600
-                    focus:ring
-                    focus:ring-orange-300
-                    btn-outline rounded-3xl 
-                    normal-case">Select</a>
+
+                    <div className="prose-sm md:prose-base order-last mt-6">
+                        <h3 className='font-bold pb-4'>
+                            <span className='font-extrabold'>&#36;</span>12 <span className='text-gray-500 font-semibold'>/ mo</span>
+                        </h3>
+                        <a href='/' className="
+                            px-16
+                            py-2
+                            text-sm
+                            no-underline
+                            font-bold
+                            border-[3px]
+                            text-white
+                            bg-[#F53835]
+                            hover:bg-orange-600
+                            hover:border-orange-600
+                            focus:ring
+                            focus:ring-orange-300
+                            btn-outline rounded-3xl 
+                            normal-case">Select
+                        </a>
+                    </div>
 
                 </div>
                 {/* -----------Card 3 end----------- */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,60 +1,61 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [
-//     "./src/**/*.{html,js,jsx,ts,tsx}",
-
-//     'node_modules/daisyui/dist/**/*.js', 'node_modules/react-daisyui/dist/**/*.js'
-
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-
-//    // add daisyUI plugin
-//   // plugins: [
-//   //   require('daisyui'),
-//   //   require("@tailwindcss/typography")
-//   // ],
-  
-//   // daisyUI config (optional)
-//   daisyui: {
-//     styled: true,
-//     themes: true,
-//     base: true,
-//     utils: true,
-//     logs: true,
-//     rtl: false,
-//     prefix: ""
-    
-//   }
-// }
-
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+
+    'node_modules/daisyui/dist/**/*.js', 'node_modules/react-daisyui/dist/**/*.js'
+
+  ],
+  
   theme: {
     extend: {},
   },
-  //.....................................................
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#669999",
-          secondary: "#f6d860",
-          accent: "#37cdbe",
-          neutral: "#3d4451",
-          "base-100": "#ffffff",
-        },
-      },
-      "dark",
-      "cupcake",
-    ],
-    //....................................................
-  },
-  plugins: [
-    require("daisyui"),
-    require("@tailwindcss/typography")
-],
 
+  //  add daisyUI plugin
+  plugins: [
+    require('daisyui'),
+    require("@tailwindcss/typography")
+  ],
+  
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: ""
+    
+  }
 }
+
+
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {},
+//   },
+//   //.....................................................
+//   daisyui: {
+//     themes: [
+//       {
+//         mytheme: {
+//           primary: "#669999",
+//           secondary: "#f6d860",
+//           accent: "#37cdbe",
+//           neutral: "#3d4451",
+//           "base-100": "#ffffff",
+//         },
+//       },
+//       // "dark",
+//       // "cupcake",
+//     ],
+//     //....................................................
+//   },
+//   plugins: [
+//     require("daisyui"),
+//     require("@tailwindcss/typography")
+// ],
+
+// }
